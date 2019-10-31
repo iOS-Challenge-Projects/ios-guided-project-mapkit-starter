30de52540e20dc755b8f415f0b74aa9fa0475ac6
Paul Solt <PaulSolt@gmail.com>
2019-10-31 12:45:18 -0400

    Ready to test our model with unit tests

---
+++ Quakes/Quakes/Quake.swift
@@ -0,0 +1,23 @@
+//
+//  Quake.swift
+//  Quakes
+//
+//  Created by Paul Solt on 10/31/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+// MKAnnotation requires we subclass NSObject
+// Allows us to show data on the a map
+
+class Quake: NSObject, Decodable {
+	
+	// mag
+	// place
+	// time
+	// lat, long
+	
+	
+	
+}

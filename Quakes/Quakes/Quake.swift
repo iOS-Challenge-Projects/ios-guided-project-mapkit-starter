e37ef659389b7bea56fcee38c69164d809c8e459
Paul Solt <PaulSolt@gmail.com>
2019-10-31 13:30:30 -0400

    Parsing a full Quake object

--- Quakes/Quakes/Quake.swift
+++ Quakes/Quakes/Quake.swift
@@ -13,24 +13,39 @@ import Foundation
 
 class Quake: NSObject, Decodable {
 	
-	// mag
 	// place
 	// time
 	// lat, long
-	
+
 	let magnitude: Double
+	let place: String
+	let time: Date
+	let latitude: Double
+	let longitude: Double
+	
 	
 	enum QuakeCodingKeys: String, CodingKey {
 		case magnitude = "mag"
 		case properties
+		case place
+		case time
+		case latitude
+		case longitude
+		case geometry
+		case coordinates
 	}
 	
 	required init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: QuakeCodingKeys.self)
 		let properties = try container.nestedContainer(keyedBy: QuakeCodingKeys.self, forKey: .properties)
-		
+		let geometry = try container.nestedContainer(keyedBy: QuakeCodingKeys.self, forKey: .geometry)
+		var coordinates = try geometry.nestedUnkeyedContainer(forKey: .coordinates)
 		
 		self.magnitude = try properties.decode(Double.self, forKey: .magnitude)
+		self.place = try properties.decode(String.self, forKey: .place)
+		self.time = try properties.decode(Date.self, forKey: .time)
+		self.longitude = try coordinates.decode(Double.self)
+		self.latitude = try coordinates.decode(Double.self)
 		
 		super.init()
 	}

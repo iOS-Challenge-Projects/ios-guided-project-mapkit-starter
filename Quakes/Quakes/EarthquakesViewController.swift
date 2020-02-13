af704ef73b0f4b933d885a8b09ad02c680f166f5
Paul Solt <PaulSolt@gmail.com>
2020-02-13 14:18:03 -0500

    Added the QuakeDetailView and showing data magnitude using colors

--- Quakes/Quakes/EarthquakesViewController.swift
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -19,9 +19,13 @@ class EarthquakesViewController: UIViewController {
 	// NOTE: You need to import MapKit to link to MKMapView
 	@IBOutlet var mapView: MKMapView!
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "QuakeView")
+        
+        
         quakeFetcher.fetchQuakes { (quakes, error) in
             if let error = error {
                 print("Error fetching quakes: \(error)")
@@ -37,7 +41,43 @@ class EarthquakesViewController: UIViewController {
             DispatchQueue.main.async {
                 self.mapView.addAnnotations(quakes)
                  
+                guard let quake = quakes.first else { return }
+                let coordinateSpan = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
+                let region = MKCoordinateRegion(center: quake.coordinate, span: coordinateSpan)
+                
+                self.mapView.setRegion(region, animated: true)
             }
         }
 	}
+}
+
+extension EarthquakesViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let quake = annotation as? Quake else {
+            fatalError("Only Quake objects are supported right now")
+        }
+        
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "QuakeView") as? MKMarkerAnnotationView else {
+            fatalError("Missing a registered annotationview")
+        }
+        
+        annotationView.glyphImage = UIImage(named: "QuakeIcon")
+        
+        if quake.magnitude >= 5 {
+            annotationView.markerTintColor = .red
+        } else if quake.magnitude >= 3 && quake.magnitude < 5 {
+            annotationView.markerTintColor = .orange
+        } else {
+            annotationView.markerTintColor = .yellow
+        }
+        
+        annotationView.canShowCallout = true
+        let detailView = QuakeDetailView()
+        detailView.quake = quake
+        annotationView.detailCalloutAccessoryView = detailView
+        
+        
+        return annotationView
+    }
 }
